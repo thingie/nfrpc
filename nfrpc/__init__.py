9ed35bfaeef5a38e00a5dfaf2412641ffc3c2014
Honza Bartoš <pony@bahnhof.cz>
2014-03-24 09:01:25 +0100

    Add \'nice\' interface

---
+++ nfrpc/__init__.py
@@ -0,0 +1,17 @@
+import frpc
+
+class ParseErrorException(Exception):
+    def __init__(self, msg):
+        self.value = msg
+    def __str__(self):
+        return repr(self.value)
+
+def parseFRPCMessage(msg):
+    try:
+        msg, len = frpc.convertMsg(msg)
+        return msg
+    except Exception, e:
+        raise ParseErrorException(e)
+
+def createFRPCMessage(msg):
+    raise 'Not implemented yet'
